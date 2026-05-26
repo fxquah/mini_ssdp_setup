6c3cbb277f88087d5194967f240d5ad281b48f09
fxquah <fxqh20@gmail.com>
2026-05-26 22:28:17 +0100

    add state machine

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,11 +1,11 @@
-data "archive_file" "lambda_zip" {
+data "archive_file" "trigger_ingestion_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_src"
   output_path = "${path.module}/lambda_src/handler.zip"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "ssdp-lambda-exec-${var.environment}"
+resource "aws_iam_role" "trigger_ingestion_exec" {
+  name = "ssdp-trigger-ingestion-exec-${var.environment}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -17,27 +17,27 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_exec.name
+resource "aws_iam_role_policy_attachment" "trigger_ingestion_basic" {
+  role       = aws_iam_role.trigger_ingestion_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-module "lambda" {
+module "trigger_ingestion" {
   source = "./modules/lambda"
 
-  function_name    = "ssdp-trigger-handler-${var.environment}"
-  role             = aws_iam_role.lambda_exec.arn
+  function_name    = "ssdp-trigger-ingestion-${var.environment}"
+  role             = aws_iam_role.trigger_ingestion_exec.arn
   handler          = "handler.handler"
   runtime          = "python3.12"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = data.archive_file.trigger_ingestion_zip.output_path
+  source_code_hash = data.archive_file.trigger_ingestion_zip.output_base64sha256
 }
 
 resource "aws_lambda_permission" "allow_s3" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = module.lambda.lambda_function_name
-  qualifier     = module.lambda.lambda_alias_name
+  function_name = module.trigger_ingestion.lambda_function_name
+  qualifier     = module.trigger_ingestion.lambda_alias_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.main.arn
 }
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_notification" "trigger" {
   bucket = aws_s3_bucket.main.id
 
   lambda_function {
-    lambda_function_arn = module.lambda.lambda_alias_arn
+    lambda_function_arn = module.trigger_ingestion.lambda_alias_arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "trigger.json"
   }
---
+++ terraform/state_machine.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_role" "sfn_exec" {
+  name = "ssdp-sfn-exec-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "states.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "sfn_exec_policy" {
+  name = "ssdp-sfn-exec-policy-${var.environment}"
+  role = aws_iam_role.sfn_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "lambda:InvokeFunction"
+        Resource = module.trigger_ingestion.lambda_alias_arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogDelivery",
+          "logs:PutLogEvents",
+          "logs:GetLogDelivery",
+          "logs:UpdateLogDelivery",
+          "logs:DeleteLogDelivery",
+          "logs:ListLogDeliveries",
+          "logs:PutResourcePolicy",
+          "logs:DescribeResourcePolicies",
+          "logs:DescribeLogGroups"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_log_group" "etl_rd_state_machine" {
+  name              = "/aws/state-machine/ssdp-etl-rd-${var.environment}"
+  retention_in_days = 14
+}
+
+resource "aws_sfn_state_machine" "etl_rd" {
+  name     = "ssdp-etl-rd-${var.environment}"
+  role_arn = aws_iam_role.sfn_exec.arn
+  publish  = true
+
+  definition = templatefile("${path.module}/state-machine-definitions/etl-rd.asl.json", {
+    TriggerIngestionLambda = module.trigger_ingestion.lambda_alias_arn
+  })
+
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.etl_rd_state_machine.arn}:*"
+    include_execution_data = true
+    level                  = "ALL"
+  }
+}
