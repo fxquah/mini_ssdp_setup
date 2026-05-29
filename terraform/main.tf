d53feff7f1c617db9602128279ec2e6dcfa899e8
fxquah <fxqh20@gmail.com>
2026-05-30 00:01:26 +0100

    checkpoint: remove unneeded bits

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,22 +16,3 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-
-resource "aws_lambda_layer_version" "pyspy" {
-  filename            = "layers/py-spy-layer.zip"
-  layer_name          = "py-spy"
-  compatible_runtimes = ["python3.11"]
-}
-
-resource "aws_lambda_function" "my_lambda" {
-  # ...existing config...
-  layers = [aws_lambda_layer_version.pyspy.arn]
-
-  environment {
-    variables = {
-      ENABLE_PYSPY        = "0"   # flip to "1" without redeploying
-      PYSPY_S3_BUCKET     = aws_s3_bucket.profiles.bucket
-    }
-  }
-}
